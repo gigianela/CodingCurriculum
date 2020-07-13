2e68c4fa69e98a8c13a2e4fecf5d4b98807293ac
Gianela <gsabino@redventures.com>
2020-07-12 23:10:21 -0400

    week4

---
+++ 01_Core/week04/submissions/e-commerce site/e-commerce.js
@@ -0,0 +1,10 @@
+// mobile nav fxn
+
+function mobileNav() {
+    var x= document.getElementById("navMobile");
+    if (x.style.display === "block") {
+    x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
